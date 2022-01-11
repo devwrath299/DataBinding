dc5202bd3a3ea45fafdb3b143494544becd7d281
dev <devwrath1999@gmail.com>
2022-01-11 21:55:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+          var q= Quoting("Hello I am Dev","Ramkrishan")
+          binding.quote=q
+
+    }
+}
---
+++ app/src/main/java/com/example/databinding/Quoting.kt
@@ -0,0 +1,3 @@
+package com.example.databinding
+
+data class Quoting(var text:String, var author:String)
